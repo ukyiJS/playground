ddcfc841fa299259b30bd733ec62380281c4b925
ukyi <devcat.js@gmail.com>
2023-01-08 21:55:21 +0900

    ✨ homeRoute name 추가

--- vue/custom-life-cycle/src/App.vue
+++ vue/custom-life-cycle/src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
+import { homeRoute } from '@/router/routes/home';
 import HelloWorld from './components/HelloWorld.vue';
 </script>
 
@@ -8,13 +9,13 @@ import HelloWorld from './components/HelloWorld.vue';
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld v-focus msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">
+        <RouterLink :to="{ name: homeRoute.home }">
           Home
         </RouterLink>
-        <RouterLink to="/about">
+        <RouterLink :to="{ name: homeRoute.about }">
           About
         </RouterLink>
       </nav>
