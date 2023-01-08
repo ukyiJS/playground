8efddcf59aba92730d0befb7fb604716d14c6015
ukyi <devcat.js@gmail.com>
2023-01-08 21:54:57 +0900

    ✨ getRoutes 추가

--- vue/custom-life-cycle/src/router/index.ts
+++ vue/custom-life-cycle/src/router/index.ts
@@ -1,21 +1,20 @@
+import type { RouteRecordRaw } from 'vue-router';
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '@/pages/HomePage.vue';
+
+const getRoutes = (): RouteRecordRaw[] => {
+  const symbol = Symbol.for('routes');
+  const modules = import.meta.glob<ObjectLiteral<{ [p: symbol]: RouteRecordRaw[] }>>('/src/router/routes/!(index).[j,t]s', { eager: true });
+  return Object.values(modules).reduce<RouteRecordRaw[]>((acc, module) => {
+    const routes = Object.values(module).map(routeSymbols => {
+      const routes = routeSymbols[symbol];
+      delete routeSymbols[symbol];
+      return routes;
+    });
+    return acc.concat(...routes);
+  }, []);
+};
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('@/pages/AboutPage.vue'),
-    },
-  ],
+  routes: getRoutes(),
 });
