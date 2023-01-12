8e6bb82451a19be67744f8361a905af9ce3b977c
ukyi <devcat.js@gmail.com>
2023-01-12 22:08:05 +0900

    ⬇️ downgrade vue 2.7

--- vue/custom-life-cycle/vite.config.ts
+++ vue/custom-life-cycle/vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
+import vue from '@vitejs/plugin-vue2';
+import vueJsx from '@vitejs/plugin-vue2-jsx';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
