3b3f21f4189b006468656a067203e130115dcac8
ukyi <ukyi.js@gmail.com>
2023-04-22 11:13:11 +0900

    🏷️ NumberRange type 추가

---
+++ shared/utility-types/numberRange.d.ts
@@ -0,0 +1,9 @@
+type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc['length']]>
+
+/**
+ * @example
+ *   type Result = NumberRange<1, 10>; // 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+ */
+export type NumberRange<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>;
