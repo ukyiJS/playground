17de7f99ba1698f12586f425452979db5f6e65b2
ukyi <ukyi.js@gmail.com>
2023-08-13 14:55:20 +0900

    ♻️ refactor test code

--- packages/react/template/src/__tests__/HelloWorld.spec.tsx
+++ packages/react/template/src/__tests__/HelloWorld.spec.tsx
@@ -1,5 +1,5 @@
-import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
+import { render, screen } from './utils';
 import App from '@/App';
 
 describe('HelloWorld', () => {
